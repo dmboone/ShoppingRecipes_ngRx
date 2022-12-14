70049f386e5d6643bb4b64ad763175afb36436ed
Destiny Boone <destaj.db@gmail.com>
2022-12-14 12:50:52 -0500

    dispatching actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,6 +5,8 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action { // using the Action interface to build our shopping list actions
     readonly type = ADD_INGREDIENT; // must have this as per the Action interface
-    payload: Ingredient; // the payload is specific to the action; it's like the data attached to an action
+
+    // the payload is specific to the action; it's like the data attached to an action
     // in this case the AddIngredient action would naturally include the ingredient you want to add as the payload
+    constructor(public payload: Ingredient){}
 }
