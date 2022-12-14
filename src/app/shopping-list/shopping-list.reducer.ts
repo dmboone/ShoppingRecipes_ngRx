d84067dfdd610e33c916ef064b6ad2448e424a5e
Destiny Boone <destaj.db@gmail.com>
2022-12-14 11:44:39 -0500

    adding logic to the reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,4 @@
+import { Action } from "@ngrx/store";
 import { Ingredient } from "../shared/ingredient.model";
 
 const initialState = { // states should normally be javascript objects
@@ -7,6 +8,18 @@ const initialState = { // states should normally be javascript objects
     ]
 };
 
-export function shoppingListReducer(state = initialState, action){ // our reducer will take in the current state and the action we want to perform 
-    // notice above that state is given a default value of initialState
+// our reducer will take in the current state and the action we want to perform 
+// notice that state is given a default value of initialState
+export function shoppingListReducer(state = initialState, action: Action){ 
+    switch (action.type){
+        case 'ADD_INGREDIENT': // convention is to use all uppercase for your action identifiers, but the names are up to you
+            // must never edit the existing state, instead return a new object which will replace the old state
+            return {
+                ...state, // good practice to call spread operator here to make sure you copy everything from previous state but in this case this line isn't necessary just good habit
+                ingredients: [
+                    ...state.ingredients, // spread operator copies everything from current state (i.e. the current list of ingredients) and adds to this ingredients array we are creating to return
+                    action // lastly we will add the new ingredient; this line isn't finished yet
+                ]
+            }
+    }
 }
