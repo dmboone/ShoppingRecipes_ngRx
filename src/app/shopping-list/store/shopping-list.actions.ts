3394797ee891cd704bcff97952de85c3f5988388
Destiny Boone <destaj.db@gmail.com>
2022-12-14 13:08:09 -0500

    preparing update and delete actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action { // using the Action interface to build our shopping list actions
     readonly type = ADD_INGREDIENT; // must have this as per the Action interface
@@ -18,4 +20,20 @@ export class AddIngredients implements Action {
     constructor(public payload: Ingredient[]){}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+    readonly type = UPDATE_INGREDIENT;
+
+    constructor(public payload: {index: number, ingredient: Ingredient}){}
+}
+
+export class DeleteIngredient implements Action {
+    readonly type = DELETE_INGREDIENT;
+
+    constructor(public payload: number){}
+}
+
+export type ShoppingListActions = 
+AddIngredient 
+| AddIngredients 
+| UpdateIngredient 
+| DeleteIngredient;
