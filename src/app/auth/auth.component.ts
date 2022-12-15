830cfddfa2ad355de2c6ff025d2f25373d506ace
Destiny Boone <destaj.db@gmail.com>
2022-12-15 15:31:11 -0500

    managing ui state in ngrx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import {Component, ComponentFactoryResolver, OnDestroy, ViewChild} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -13,7 +13,7 @@ import * as AuthActions from './store/auth.actions';
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent implements OnDestroy{
+export class AuthComponent implements OnInit, OnDestroy{
     isLoginMode = true;
     isLoading = false;
     error: string = null;
@@ -22,6 +22,13 @@ export class AuthComponent implements OnDestroy{
     private closeSub: Subscription;
 
     constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver, private store: Store<fromApp.AppState>){}
+
+    ngOnInit(): void {
+        this.store.select('auth').subscribe(authState => { // ngRx implementation to handle observable
+            this.isLoading = authState.loading;
+            this.error = authState.authError;
+        });
+    }
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode; // just switches the isLoginMode value
@@ -48,21 +55,22 @@ export class AuthComponent implements OnDestroy{
         else{ // not logging in so need so run signup method
             authObs = this.authService.signup(email, password);
         }
-
-        authObs.subscribe( // subscribe to the authentication observable whether we are in logging in or signing up
-            resData =>{
-                console.log(resData);
-                this.isLoading = false;
-                this.router.navigate(['/recipes']); // navigate to recipes route on successful authentication
-            },
-            errorMessage => // specifics of errorMessage is now getting handled in the auth.service through a catchError pipe
-            {
-                console.log(errorMessage);
-                this.error = errorMessage;
-                this.showErrorAlert(errorMessage);
-                this.isLoading = false;
-            }
-        );
+        
+        // replacing this code with ngRx implementation instead (see ngOnInit)
+        // authObs.subscribe( // subscribe to the authentication observable whether we are in logging in or signing up
+        //     resData =>{
+        //         console.log(resData);
+        //         this.isLoading = false;
+        //         this.router.navigate(['/recipes']); // navigate to recipes route on successful authentication
+        //     },
+        //     errorMessage => // specifics of errorMessage is now getting handled in the auth.service through a catchError pipe
+        //     {
+        //         console.log(errorMessage);
+        //         this.error = errorMessage;
+        //         this.showErrorAlert(errorMessage);
+        //         this.isLoading = false;
+        //     }
+        // );
 
         form.reset(); // always reset form on submission
     }
